7bbbea6b7bbc4c7ef867f2424e3aae184814df25
Andy Fang <andy@mingtak.com>
2016-06-01 05:45:43 +0800

    add googleAD to head tag with viewlet

--- src/i8d/content/viewlet/viewlet.py
+++ src/i8d/content/viewlet/viewlet.py
@@ -4,19 +4,22 @@ from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 
 
 class EffectiveDateRange(base.ViewletBase):
-    """ """
+    """  """
 
 class ManageProducts(base.ViewletBase):
-    """ """
+    """  """
 
 class HeaderContainer(base.ViewletBase):
-    """ """
+    """  """
 
 class ShowUID(base.ViewletBase):
-    """ """
+    """  """
 
 class CoverHeroViewlet(base.ViewletBase):
-    """ """
+    """  """
 
 class ServiceExplanation(base.ViewletBase):
-    """ """
+    """  """
+
+class GoogleAD(base.ViewletBase):
+    """  """
